0793f5546ff46f40bc99a235e86ac28d84ae6b33
bugaivo <54854694+bugaivo@users.noreply.github.com>
2023-01-25 16:00:38 +0200

    Update and rename initial_commit.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,92 @@
+#provider azure
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+#create rg
+resource   "azurerm_resource_group"   "rg"   { 
+   name   =   "my-first-terraform-rg" 
+   location   =   "northeurope" 
+ } 
+resource   "azurerm_resource_group"   "rg2"   { 
+   name   =   "my-second-terraform-rg" 
+   location   =   "northeurope" 
+ } 
+ #create vnet
+resource   "azurerm_virtual_network"   "myvnet"   { 
+   name   =   "my-vnet" 
+   address_space   =   [ "10.0.0.0/16" ] 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+ } 
+#create subnet
+ resource   "azurerm_subnet"   "frontendsubnet"   { 
+   name   =   "frontendSubnet" 
+   resource_group_name   =    azurerm_resource_group.rg.name 
+   virtual_network_name   =   azurerm_virtual_network.myvnet.name 
+   address_prefix   =   "10.0.1.0/24" 
+ } 
+
+
+# Create public IPs
+resource   "azurerm_public_ip"   "myvm1publicip"   { 
+   name   =   "pip1" 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+   allocation_method   =   "Dynamic" 
+   sku   =   "Basic" 
+ } 
+
+# Create Network Security Group and rule
+
+
+# Create network interface
+resource   "azurerm_network_interface"   "myvm1nic"   { 
+   name   =   "myvm1-nic" 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+
+   ip_configuration   { 
+     name   =   "ipconfig1" 
+     subnet_id   =   azurerm_subnet.frontendsubnet.id 
+     private_ip_address_allocation   =   "Dynamic" 
+   } 
+ }
+
+
+# Define VM
+
+resource   "azurerm_windows_virtual_machine"   "example"   { 
+   name                    =   "myvm1"   
+   location                =   "northeurope" 
+   resource_group_name     =   azurerm_resource_group.rg.name 
+   network_interface_ids   =   [ azurerm_network_interface.myvm1nic.id ] 
+   size                    =   "Standard_B1s" 
+   admin_username          =   "adminuser" 
+   admin_password          =   "Password123!" 
+
+   source_image_reference   { 
+     publisher   =   "MicrosoftWindowsServer" 
+     offer       =   "WindowsServer" 
+     sku         =   "2019-Datacenter" 
+     version     =   "latest" 
+   } 
+
+   os_disk   { 
+     caching             =   "ReadWrite" 
+     storage_account_type   =   "Standard_LRS" 
+   } 
+ }
